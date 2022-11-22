90c7f23f8ce48c6dfdf5589371012a82ebe4fdd8
ryuga0719 <ryuga.private@gmail.com>
2022-11-22 15:19:44 +0900

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import Fluctuation from "./modules/Fluctuation";
+
+const arr = [
+  new Fluctuation(0.4, ".fire.--red", "--fluctuation-red"),
+  new Fluctuation(0.65, ".fire.--blue", "--fluctuation-blue"),
+  new Fluctuation(0.96, ".fire.--yellow", "--fluctuation-yellow"),
+];
